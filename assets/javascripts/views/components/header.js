3422713e4de392146902b59c28accfb582722be5
enamakel <me@steven.pw>
2015-02-15 11:54:43 +0300

    Some adjustments to make the header seem seamless;

--- assets/javascripts/views/components/header.js
+++ assets/javascripts/views/components/header.js
@@ -11,10 +11,10 @@ module.exports = Backbone.View.extend({
 	 * Hide the Slider
 	 */
 	hide: function() {
+		this.shown = false;
 		this.$sliderNav.stop().animate({ right: -1 * this.sliderAnimateWidth });
 
-		var width = this.$window.width();
-		this.$main.stop().animate({ width: width});
+		this.expandMain();
 	},
 
 
@@ -22,15 +22,42 @@ module.exports = Backbone.View.extend({
 	 * Show the Slider
 	 */
 	show: function() {
+		this.shown = true;
 		this.$sliderNav.stop().animate({ right: 0 });
 
+		this.shirnkMain();
+	},
+
+
+	/**
+	 * Expands the main body
+	 */
+	expandMain: function() {
+		var width = this.$window.width();
+		var that = this;
+		this.$main.stop().animate({width: width}, function() {
+
+			/* Remove the width style set by jQuery's animate. */
+			that.$main.css("width", "");
+		});
+	},
+
+
+	/**
+	 * Shrinks the mainbody
+	 */
+	shirnkMain: function() {
 		var width = this.$window.width();
 		this.$main.stop().animate({ width: width - this.sliderAnimateWidth});
 	},
 
+
 	initialize: function() {
+		this.shown = false;
 		this.$main = $("main");
 		this.$window = $(window);
 		this.$sliderNav = $("#slider-nav");
+
+		// this.$window.on("resize", this.hide);
 	}
 });
