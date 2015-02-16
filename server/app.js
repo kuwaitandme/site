15d33c6d4160c2d926ce4cb61db7085c4fd9f6e5
enamakel <me@steven.pw>
2015-02-16 18:22:35 +0300

    force https on production;

--- server/app.js
+++ server/app.js
@@ -21,7 +21,8 @@ else app.config = {
 	hostname: "localhost",
 }
 
-/* International langauge support */
+
+/* International language support */
 i18n.configure({
 	cookie: 'lang',
 	defaultLocale: 'en',
@@ -29,6 +30,7 @@ i18n.configure({
 	locales:['en', 'ar', 'in']
 });
 app.use(i18n.init);
+
 
 /* view engine setup */
 app.set('views', path.join(__dirname, 'views'));
@@ -41,24 +43,40 @@ app.use(bodyParser.urlencoded({ extended: false }));
 var cacheTime = 86400000*7;
 app.use(express.static(__dirname + '/public', { maxAge: cacheTime }));
 
+
 /* Cookie and sessions */
 app.use(cookieParser());
-// app.use(expressSession({
-// 	name: 'sess',
-// 	proxy: true,
-// 	resave: true,
-// 	saveUninitialized: true,
-// 	secret: 'e2nURBBjy1ieSbWP'
-// }));
+app.use(expressSession({
+	name: 'sess',
+	proxy: true,
+	resave: true,
+	saveUninitialized: true,
+	secret: config.SessionSecret
+}));
 
-/* Initialize Passport */
+
+/* Initialize Passport User authentication */
 app.use(passport.initialize());
 app.use(passport.session());
 var initPassport = require('./controllers/auth/passport/init');
 initPassport(passport);
 
+
+/* If we are in production mode, redirect all URLs to https */
+if (app.config.mode == 'production') {
+	app.use(function(req, res, next) {
+		if (req.protocol === 'http') {
+			var newUrl = 'https://' + req.get('host') + req.originalUrl;
+			return res.redirect(newUrl);
+		}
+		next();
+	});
+}
+
+
 /* Setup the different routes */
 app.use('/', routes);
+
 
 /* None of the URL matched, so return 404  */
 app.use(function(req, res, next) {
@@ -72,7 +90,16 @@ mongoose.connect('mongodb://localhost/kuwaitandme');
 
 
 /* Setup environment specific functions */
-if (app.config.mode == 'development') {
+if (app.config.mode == 'production') {
+	/* production error handler, no stacktraces leaked to user */
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		return res.render('error', {
+			message: err.message,
+			error: {}
+		});
+	});
+} else {
 	app.locals.pretty = true;
 
 	/* development error handler will print stacktrace */
@@ -81,15 +108,6 @@ if (app.config.mode == 'development') {
 		return res.render('error', {
 			message: err.message,
 			error: err
-		});
-	});
-} else {
-	/* production error handler, no stacktraces leaked to user */
-	app.use(function(err, req, res, next) {
-		res.status(err.status || 500);
-		return res.render('error', {
-			message: err.message,
-			error: {}
 		});
 	});
 }
