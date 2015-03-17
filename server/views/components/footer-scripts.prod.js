daeb3c47786a3666c06b00302b0fd58e99ca81ae
enamakel <me@steven.pw>
2015-03-17 21:33:54 +0300

    updated footer scripts

--- server/views/components/footer-scripts.prod.js
+++ server/views/components/footer-scripts.prod.js
@@ -4,47 +4,47 @@
  * get cached properly.
  */
 window.scripts = [{
-	name:"jquery",
+	name: "lib:jquery",
 	remoteSrc: "//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js",
 	localSrc: "/javascripts/vendor/jquery.min.js",
 },{
-	name:"jquery-transit",
+	name: "lib:jquery-transit",
 	remoteSrc: "//cdnjs.cloudflare.com/ajax/libs/jquery.transit/0.9.12/jquery.transit.min.js",
 	localSrc: "/javascripts/vendor/jquery.transit.min.js",
 },{
-	name:"underscore",
+	name: "lib:underscore",
 	remoteSrc: "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min.js",
 	localSrc: "/javascripts/vendor/underscore.min.js",
 },{
-	name:"backbone",
+	name: "lib:backbone",
 	remoteSrc: "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min.js",
 	localSrc: "/javascripts/vendor/backbone.min.js",
 },{
-	name:"modernizr",
+	name: "lib:modernizr",
 	remoteSrc: "//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js",
 	localSrc: "/javascripts/vendor/modernizr.min.js",
 },{
-// 	name:"2checkout",
+// 	name: "lib:2checkout",
 // 	remoteSrc: "//www.2checkout.com/checkout/api/2co.min.js",
 // 	localSrc: "/javascripts/vendor/2co.min.js",
 // },{
-	name:"dropzone",
+	name: "lib:dropzone",
 	remoteSrc: "//cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/dropzone.min.js",
 	localSrc: "/javascripts/vendor/dropzone.min.js",
 },{
-	name:"masonry",
+	name: "lib:masonry",
 	remoteSrc: "//cdnjs.cloudflare.com/ajax/libs/masonry/3.2.2/masonry.pkgd.min.js",
 	localSrc: "/javascripts/vendor/masonry.pkgd.min.js",
 },{
-	name:"google-maps",
+	name: "lib:google-maps",
 	remoteSrc: "//maps.googleapis.com/maps/api/js?v=3.exp&callback=initializeGmap",
 	localSrc: "/javascripts/vendor/google.maps.js",
 },{
-	name:"jquery-imagesloaded",
+	name: "lib:jquery-imagesloaded",
 	remoteSrc: "//cdnjs.cloudflare.com/ajax/libs/jquery.imagesloaded/3.1.8/imagesloaded.pkgd.min.js",
 	localSrc: "/javascripts/vendor/imagesloaded.min.js",
 },{
-	name:"app",
+	name: "app:script",
 	remoteSrc: "/javascripts/build/app.min.js",
 	localSrc: "/javascripts/build/app.min.js",
 }]
@@ -79,7 +79,7 @@ for (var i = scripts.length; i >= 1; i--) {
 
 	if(typeof Storage !== "undefined") {
 		/* Check for the script in our cache */
-		var scriptCache = localStorage.getItem("script-" + script.name);
+		var scriptCache = localStorage.getItem(script.name);
 
 		/* If the cache exists, then read from it; Otherwise load the script
 		 * normally */
