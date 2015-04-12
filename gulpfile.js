7457356c3fb39866a3c569ef9924484f023981cb
enamakel <me@steven.pw>
2015-04-12 12:03:37 +0300

    updated gulp script to compile on server deployment

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,8 @@ gulp.task('html:minified', ['jade:minified']);
 gulp.task('js', ['coffee']);
 gulp.task('js:minified', ['coffee:minified']);
 
+gulp.task('minify', ['js:minified', 'css:minified', 'html:minified'])
 gulp.task('build', ['js', 'css', 'html']);
-gulp.task('deploy', ['js:minified', 'css:minified', 'html:minified', 'server']);
 
+gulp.task('deploy', ['build', 'minify', 'server']);
 gulp.task('default', ['build', 'watch']);
