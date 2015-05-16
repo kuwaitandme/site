7c6e92387f3c97abdf616a0809ff57e0fb82f3b6
Steven Enamakel <me@steven.pw>
2015-05-16 18:57:19 +0530

    Added server task into gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,6 @@ gulp.task("js:minified", ["coffee:minified"]);
 gulp.task("minify", ["js:minified", "css:minified"])
 gulp.task("build", ["js", "css", "html", "bower"]);
 
-// gulp.task("deploy", ["build", "minify", "server"]);
-gulp.task("deploy", ["build", "minify"]);
+gulp.task("deploy", ["build", "minify", "server"]);
+// gulp.task("deploy", ["build", "minify"]);
 gulp.task("default", ["build", "watch"]);
