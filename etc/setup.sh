c96ef2dac5c9894daf4ba1ced0b69a731f3f9391
Steven Enamakel <me@steven.pw>
2015-07-25 22:41:59 +0530

    update again

--- etc/setup.sh
+++ etc/setup.sh
@@ -1,30 +1,3 @@
 #!/bin/bash
 
 yum update -y
-yum install -y
-
-echo "Installing Kuwait & Me"
-echo "Waking up our installation Wizard (wiz)."
-sleep 2
-echo "Instructing wiz to check your system now."
-sleep 3
-echo
-echo "wiz> Hello System!"
-sleep 2
-echo "sys> hello"
-sleep 1
-echo "wiz> Ok system. Check for program cookie"
-sleep 2
-echo "sys> muffin"
-sleep 1
-echo "wiz> Hmm.... "
-sleep 0.5
-echo "wiz> ...... "
-sleep 0.5
-echo "wiz> ... "
-sleep 2
-echo "wiz> I asked your system for a cookie but it gave me a muffin!"
-sleep 3
-echo "wiz> I'm pretty sure this means your system is broken.."
-sleep 3
-echo "wiz> So I guess follow the instructions in ../INSTALL.md"
